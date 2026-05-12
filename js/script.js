e8a04aebf71a97b67610508ba925a2a5d0d67fae
marisgit-dev <azigamarita@gmail.com>
2026-05-12 11:50:28 -0400

    Added a .env file to secure my information

--- js/script.js
+++ js/script.js
@@ -1,5 +1,14 @@
-const axios = require('axios');
+const name = document.getElementById('name');
+const email = document.getElementById('email');
+const subject = document.getElementById('subject');
+const message = document.getElementById('message');
 
-async function sendEmail(name, email, subject, message) {
+const sendEmail = document.getElementById('sendEmail');
+/**
+ * A function that sends a email so new visitors can get in touch. 
+ * This is done using axios to get a response.
+ */
+
+sendEmail.addEventListener('click', (e) => {
     
-}
+});
