5e355741df8c77ac48ac19bcb86bae862ee163d5
marisgit-dev <azigamarita@gmail.com>
2026-05-26 21:20:34 -0400

    added js and css for navbar

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+window.addEventListener("scroll", () => {
+  const nav = document.getElementById("navHeader");
+  const logo = document.getElementById("logo");
+  const about = document.getElementById("aboutLink");
+  const projects = document.getElementById("projectsLink");
+  const contact = document.getElementById("contactLink");
 
+  if (window.scrollY > 50) {
+    nav.classList.add("scrolled");
+    logo.classList.add("scrolled");
+    about.classList.add("scrolled");
+    projects.classList.add("scrolled");
+    contact.classList.add("scrolled");
+  } else {
+    nav.classList.remove("scrolled");
+    logo.classList.remove("scrolled");
+    about.classList.remove("scrolled");
+    projects.classList.remove("scrolled");
+    contact.classList.remove("scrolled");
+  }
+});
